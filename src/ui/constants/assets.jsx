e0a2c532104e589c5f8cfa53c23eac5fbd28033d
akif <su92-bssem-f22-118@superior.edu.pk>
2025-09-30 16:35:44 +0500

    feat(auth): edit

--- src/ui/constants/assets.jsx
+++ src/ui/constants/assets.jsx
@@ -1,16 +1,11 @@
-// utils/constants/assets.js
 export const IMAGES = {
-    loginBanner: "src/assets/images/logoinbanner.jpg",
-    
+    loginBanner: "src/assets/images/logoinbanner.jpg",    
   }
-  
   export const ICONS = {
     brandLogo: "src/assets/icons/logintitle.svg",
     appicon: "src/assets/icons/apptitle.svg",
-    dashboad:"src/assets/icons/mage_dashboard-fill (1).svg",
-    notication:"src/assets/icons/basil_notification-on-solid (1).svg",
+    dashboad:"src/assets/icons/magedashboardfill .svg",
+    notication:"src/assets/icons/notificationdashboard.svg",
     user:"src/assets/icons/solar_user-bold.svg",
-    vaccine:"src/assets/icons/covid_vaccine-protection-syringe (1).svg",
-
+    vaccine:"src/assets/icons/dashboardinjection.svg",
   }
-  
