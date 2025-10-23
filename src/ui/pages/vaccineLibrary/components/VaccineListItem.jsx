171bac9a288a6282ded4233e12a6a34ecd81eb09
usmaniqbal8625 <usmaniqbal8625@gmail.com>
2025-10-23 15:51:52 +0500

    crud vaccine completed

--- src/ui/pages/vaccineLibrary/components/VaccineListItem.jsx
+++ src/ui/pages/vaccineLibrary/components/VaccineListItem.jsx
@@ -9,8 +9,8 @@ useEffect(()=>{
 
 
   const ageRange =
-    item.min_age !== null && item.max_age !== null
-      ? `${item.min_age} - ${item.max_age} years`
+    item.age_range?.min_age_months !== null && item.age_range?.max_age_months !== null
+      ? `${item.age_range.min_age_months} - ${item.age_range.max_age_months} months` // Changed to months as per data
       : "N/A";
 
   return (
@@ -19,9 +19,9 @@ useEffect(()=>{
       <div className="flex justify-center items-center w-[14%] text-gray-700 font-medium">{item.category || "N/A"}</div>
       <div className="flex justify-center items-center w-[26%] text-gray-500">{ageRange}</div>
       <div className="flex justify-center items-center w-[14%] text-gray-500">{item.type || "N/A"}</div>
-      <div className="flex justify-center items-center w-[14%]">{item.site || "N/A"}</div>
+      <div className="flex justify-center items-center w-[14%]">{item.details?.site || "N/A"}</div>
       <div className="flex justify-center items-center gap-5 w-[14%]">
-        <button className="p-2 rounded-md hover:bg-blue-100" onClick={onEdit}>
+        <button className="p-2 rounded-md hover:bg-blue-100" onClick={() => onEdit(item)}>
           <img src={ICONS.edit} alt="Edit" />
         </button>
         <button className="p-2 rounded-md hover:bg-red-100" onClick={() => onDelete(item)}>
