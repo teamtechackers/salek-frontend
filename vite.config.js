088cd8d456d7d207bb9926a3ab8300eb86ebbb62
usmaniqbal8625 <usmaniqbal8625@gmail.com>
2025-10-23 14:06:14 +0500

    import issue notifcation

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+   server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000
+  }
 })
